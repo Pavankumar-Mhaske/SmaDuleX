ba535397aa16fd968892a949f579397fe1bc8b11
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-25 15:35:37 +0530

    EditTodo: JSX added .

---
+++ client/src/components/EditTodo.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * @param todo - Todo Object to fill initial values.
+ * @param editTodo - To make a rendering decision (State).
+ * @param setEditTodo - function (Update State).
+ * @returns Todo Updation Form with values populated.
+ */
+
+const EditTodo = () => {
+  return (
+    <>
+      <div
+        className={`w-full h-screen absolute top-0 left-0 `}
+        style={{ background: "rgba(0, 0, 0, 0.8)" }}
+      >
+        <div className="w-11/12 md:w-2/3 flex flex-col m-auto relative top-[60px] md:top-1/4">
+          <button className="w-[95%] sm:w-5/6 md:w-full mx-auto py-2 px-4 bg-white bg-red-600 font-bold text-xl text-white rounded-t">
+            Cancel
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditTodo;
