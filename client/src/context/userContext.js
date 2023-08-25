5042ae9f80b198ebed34efbc854d91b953cab558
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-25 17:11:26 +0530

    userContext done successfully

---
+++ client/src/context/userContext.js
@@ -0,0 +1,14 @@
+/**
+ * Central store created to check if user is logged in or not
+ */
+
+import { createContext } from "react";
+
+/**
+ export const userContext = createContext();
+ * 
+        Or
+*/
+
+const userContext = createContext();
+export default userContext;
