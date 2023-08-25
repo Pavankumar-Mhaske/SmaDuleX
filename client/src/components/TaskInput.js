33cd9bfe751dacdc8af66910d45a9ffbbc91aa77
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-25 19:14:20 +0530

    TaskInput done successfully.

--- client/src/components/TaskInput.js
+++ client/src/components/TaskInput.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+import Task from "./Task";
 /**
  *
  * @param tasks - Array (State).
@@ -5,14 +8,45 @@
  * @returns Task Collection and Task Input Element.
  */
 
-const TaskInput = () => {
+const TaskInput = ({ tasks, setTasks }) => {
+  /**
+   * Used to update the task input value when input changes.
+   */
+  const [taskInput, setTaskInput] = useState("");
+
+  /**
+   * addTask() - It adds the task to tasks array.
+   *          - Sets the updated array object to tasks.
+   *         - Clears the task input value.
+   */
+
+  const addTask = (e) => {
+    e.preventDefault();
+    setTasks([...tasks, taskInput]);
+    setTaskInput("");
+  };
+
+  /**
+   * handleChange() - Updates the task value.
+   */
+
+  const handleChange = (e) => {
+    setTaskInput(e.target.value);
+  };
+
   return (
     <>
       <div className="h-24 md:h-48 border-2 rounded mb-4 py-1 md:py-2">
         <div className="mb-6 min-h-fit max-h-20 md:max-h-44 overflow-auto flex flex-wrap">
-          <h1 className="   text-[24px] sm:text-[30px] md:text-[42px] font-bold text-violet-400 pl-6">
-            Add your tasks below
-          </h1>
+          {tasks.length === 0 ? (
+            <h1 className="text-[24px] sm:text-[30px] md:text-[42px] font-bold text-violet-400 pl-6">
+              Add your tasks below
+            </h1>
+          ) : (
+            tasks.map((task, index) => (
+              <Task body={task} tasks={tasks} setTasks={setTasks} key={index} />
+            ))
+          )}
         </div>
       </div>
       <div className="w-1/2 inline">
@@ -23,9 +57,19 @@ const TaskInput = () => {
             id="taskInput"
             name="taskInput"
             placeholder="Enter your task"
+            value={taskInput}
+            onChange={handleChange}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                addTask(e);
+              }
+            }}
           />
         </label>
-        <button className=" bg-violet-600  mt-2 md:mt-0 sm:ml-2  px-5  py-2 text-md  lg:text-lg   text-white  font-medium  rounded  active:bg-violet-400  active:text-gray-500">
+        <button
+          onclick={addTask}
+          className="bg-violet-600  mt-2 md:mt-0 sm:ml-2  px-5  py-2 text-md  lg:text-lg   text-white  font-medium  rounded  active:bg-violet-400  active:text-gray-500"
+        >
           Add Task
         </button>
       </div>
