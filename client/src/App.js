ea1f4c002f977df6f7d8877c32c2095bda209957
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-27 01:01:30 +0530

    App.js done Successfully.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,51 @@
+import React, { useState } from "react";
+
+/**
+ * Importing react-router-dom components for routing the pages in application
+ */
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+/**
+ * Importing pages which has to be assigned with route
+ */
+
+import HomePage from "./pages/Homepage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+/**
+ * Importing layouts which has to be wrapped for every route
+ */
+
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
+
+/**
+ * Importing userContext to provide the context value of user to entire application.
+ */
+import userContext from "./context/userContext";
+
 const App = () => {
-  return <></>;
+  /**
+   * This will passed to userContext. Hence it is used verify if a user is logged in or not.
+   */
+  const [user, setUser] = useState(null);
+
+  return (
+    <>
+      <userContext.Provider value={{ user, setUser }}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </userContext.Provider>
+    </>
+  );
 };
 
 export default App;
