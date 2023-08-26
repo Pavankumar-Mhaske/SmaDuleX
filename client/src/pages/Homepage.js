69e911c455fa8c6b58c5965b6af193af88dd016e
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-26 23:55:48 +0530

    Homepage.js done successfully

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,49 @@
+import { userContext, useState } from "react";
+
+// router
+import { Navigate } from "react-router-dom";
+
+// context
+import { useContext } from "../context/userContext";
+
+// components
+import TodoForm from "../components/TodoForm";
+import TodoList from "../components/TodoList";
+
+const Homepage = () => {
+  /**
+   * Used to make database calls only when you update, delete, create todos. It is passed as parameters to
+   * couple of components
+   */
+  const [makeRequest, setMakeRequest] = useState(false);
+
+  /**
+   * Used to display homepage only if user is logged in else redirect to login page.
+   */
+  const { user } = useContext(userContext);
+
+  if (user) {
+    return (
+      <>
+        <h1 className="my-6 text-2xl md:text-4xl font-medium text-violet-800 text-center">
+          Create New Todo
+        </h1>
+        <div className="w-full md:w-2/3 mx-auto">
+          <TodoForm
+            task="create"
+            buttonName="Create Todo"
+            makeRequest={makeRequest}
+            setMakeRequest={setMakeRequest}
+          />
+        </div>
+        <h1 className="mt-12 mb-6 text-2xl md:text-4xl font-medium text-violet-800 text-center">
+          Your Todos!
+        </h1>
+        <TodoList makeRequest={makeRequest} setMakeRequest={setMakeRequest} />
+      </>
+    );
+  }
+  return <Navigate to="/login" />;
+};
+
+export default Homepage;
