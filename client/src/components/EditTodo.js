bacec2cc4da649afdb13e96a8293912801e3a887
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-26 12:21:07 +0530

    EditTodo.js done successfully.

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,3 +1,9 @@
+import React, { useState } from "react";
+// import components
+import TodoForm from "./TodoForm";
+
+import useNavigation from "react-router-dom";
+
 /**
  *
  * @param todo - Todo Object to fill initial values.
@@ -6,20 +12,30 @@
  * @returns Todo Updation Form with values populated.
  */
 
-const EditTodo = () => {
+const EditTodo = ({
+  todo,
+  editTodo,
+  setEditTodo,
+  makeRequest,
+  setMakeRequest,
+}) => {
+  if (!editTodo) return "";
   return (
-    <>
-      <div
-        className={`w-full h-screen absolute top-0 left-0 `}
-        style={{ background: "rgba(0, 0, 0, 0.8)" }}
-      >
-        <div className="w-11/12 md:w-2/3 flex flex-col m-auto relative top-[60px] md:top-1/4">
-          <button className="w-[95%] sm:w-5/6 md:w-full mx-auto py-2 px-4 bg-white bg-red-600 font-bold text-xl text-white rounded-t">
-            Cancel
-          </button>
-        </div>
+    <div
+      className={`w-full h-screen absolute top-0 left-0 `}
+      style={{ background: "rgba(0, 0, 0, 0.8)" }}
+      onClick={() => {
+        setEditTodo(false);
+        document.body.style.overflow = "auto";
+      }}
+    >
+      <div className="w-11/12 md:w-2/3 flex flex-col m-auto relative top-[60px] md:top-1/4">
+        <button className="w-[95%] sm:w-5/6 md:w-full mx-auto py-2 px-4 bg-white bg-red-600 font-bold text-xl text-white rounded-t">
+          Cancel
+        </button>
+        <TodoForm task="update" buttonName="Update Todo" todo={todo} makeRequest={makeRequest} setMakeRequest={setMakeRequest} setEditTodo={setEditTodo} />
       </div>
-    </>
+    </div>
   );
 };
 
