e938cd88f6aeaf55517c827a39f4d9cf441ae61a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-26 11:44:00 +0530

    TodoModal done successfully.

--- client/src/components/TodoModal.js
+++ client/src/components/TodoModal.js
@@ -1,3 +1,11 @@
+import { useContext, useEffect, useState } from "react";
+
+// import axios
+import axios from "axios";
+
+// import context
+import { useTodoContext } from "../context/userContext";
+
 /**
  *
  * @param popup - To make rendering desicion (State).
@@ -8,30 +16,62 @@
  * @returns
  */
 
-const TodoModal = () => {
-  <>
-    <div
-      className="
-        w-[95%]
-        border-2 
-        hover:border-violet-400 
-        p-2 
-        rounded
-        text-sm
-        sm:text-md
-        md:text-lg
-        xl:text-xl 
-        text-violet-800 
-        font-medium
-        m-auto
-        max-h-24
-        md:max-h-44
-        overflow-auto
-        my-4  
-    "
-    >
+const TodoModal = ({ popup, todoId, makeRequest, created, updated }) => {
+  /**
+   * It is used to pass appwrite Id in DB request parmas
+   */
+  const { user } = useContext(useTodoContext);
+
+  /**
+   * To maintain concurrency in tasks of todo. (When we have a unsuccessful update)
+   */
+  const [tasks, setTasks] = useState([]);
+
+  /**
+   * getTodoTasks() - Asynchronous Function
+   *      - Fetches all the tasks of user's todo
+   */
+
+  const getTodoTasks = async () => {
+    try {
+      // /api/v1/todos/${todoId}/tasks
+      const { response } = await axios.get(`/todo/${user.$id}/${todoId}`);
+      console.log(response);
+
+      if (response.data.todo.tasks) {
+        setTasks(response.data.todo.tasks);
+      }
+    } catch (error) {
+      console.log("Error in Fetching Tasks of Todo");
+      console.log("Error", error);
+    }
+  };
+
+  useEffect(() => {
+    getTodoTasks();
+  }, [makeRequest]);
+
+  /**
+   * Conditional rendering: Check if param pop is true and display tasks else display "".
+   */
+
+  if (!popup) return "";
+  return (
+    <div className=" w-[95%] border-2  hover:border-violet-400  p-2  rounded text-sm sm:text-md md:text-lg xl:text-xl  text-violet-800  font-medium m-auto max-h-24 md:max-h-44 overflow-auto my-4">
       <div>
-        <p>No Tasks Available</p>
+        {
+          // Conditional Rendering
+          tasks.length === 0 ? (
+            <p>No Tasks Available</p>
+          ) : (
+            tasks.map((task, index) =>
+              task ? 
+                <p className="inline-block m-1 border-2 border-violet-800 rounded p-1" key={index}>{task}</p>
+               : 
+                ""
+            ))
+        }
+        
       </div>
       <div className="flex justify-between text-base mt-4">
         <p>
@@ -49,7 +89,7 @@ const TodoModal = () => {
         </p>
       </div>
     </div>
-  </>;
+  );
 };
 
 export default TodoModal;
