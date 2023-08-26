913373d0f04add29137804b9f79665029ea0a6f9
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-26 10:58:58 +0530

    TodoForm done successfully

--- client/src/components/TodoButton.js
+++ client/src/components/TodoButton.js
@@ -5,7 +5,10 @@
 
 const TodoButton = ({ name = "Todo Button" }) => {
   return (
-    <button className=" mt-2  rounded  w-full  border-2  border-green-500  px-6  py-2  font-semibold  rounded-b  text-green-500  text-lg  active:bg-green-300  active:text-gray-600">
+    <button
+      className=" mt-2  rounded  w-full  border-2  border-green-500  px-6  py-2  font-semibold  rounded-b  text-green-500  text-lg  active:bg-green-300  active:text-gray-600"
+      type="submit"
+    >
       {name}
     </button>
   );
