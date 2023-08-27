e3f662ce009ab85148e8ad63b0eb05fd463bbd8d
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-27 11:34:41 +0530

    TodoButton: comments added for changing color.

--- client/src/components/TodoButton.js
+++ client/src/components/TodoButton.js
@@ -7,6 +7,8 @@ const TodoButton = ({ name = "Todo Button", passwordMatched }) => {
   return (
     <button
       className={`mt-2 rounded w-full border-2 px-6 py-2 font-semibold ${
+        // if passwordMatched is true then button will be green
+        // if passwordMatched is false then button will be gray
         passwordMatched
           ? `border-green-500 text-green-500 active:bg-green-300 active:text-gray-600`
           : `border-gray-400 text-gray-400 cursor-not-allowed`
