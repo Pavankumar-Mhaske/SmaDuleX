8207d245b85859bcac3abcb8b86d17fb87cc7516
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-27 11:19:58 +0530

    TodoButton : {activate only on `passwordMatched` is true.}

--- client/src/components/TodoButton.js
+++ client/src/components/TodoButton.js
@@ -3,11 +3,16 @@
  * @returns A Button element to submit the data in  form.
  */
 
-const TodoButton = ({ name = "Todo Button" }) => {
+const TodoButton = ({ name = "Todo Button", passwordMatched }) => {
   return (
     <button
-      className=" mt-2  rounded  w-full  border-2  border-green-500  px-6  py-2  font-semibold  rounded-b  text-green-500  text-lg  active:bg-green-300  active:text-gray-600"
+      className={`mt-2 rounded w-full border-2 px-6 py-2 font-semibold ${
+        passwordMatched
+          ? `border-green-500 text-green-500 active:bg-green-300 active:text-gray-600`
+          : `border-gray-400 text-gray-400 cursor-not-allowed`
+      } text-lg `}
       type="submit"
+      disabled={!passwordMatched} // button will activate only if passwordMatched is true
     >
       {name}
     </button>
