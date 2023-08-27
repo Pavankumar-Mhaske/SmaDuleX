6b3956e9ca8b167a678d383f8640b8957ebff618
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-27 23:13:12 +0530

    appwriteConfig.js: Project(setProject) Id and API Endpoint(setEndpoint) are set

--- client/src/config/appwriteConfig.js
+++ client/src/config/appwriteConfig.js
@@ -5,7 +5,7 @@ import { Client, Account } from "appwrite";
  */
 const client = new Client()
   .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
-  .setProject("[PROJECT_ID]"); // Your project ID
+  .setProject("64e8395f88b2c38e0dbc"); // Your project ID
 
 /**
  * Initialization (Configuring) of appwrite Account SDK.
