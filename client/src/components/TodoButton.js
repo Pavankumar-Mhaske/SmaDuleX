90fc8a77291e4f5ff32d3e22a1c5bb30b5a70de5
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-30 06:11:58 +0530

    TodoButton.js: Css effects added to the TodoButton

--- client/src/components/TodoButton.js
+++ client/src/components/TodoButton.js
@@ -7,15 +7,14 @@ import "../pages/customStyles.css";
 const TodoButton = ({ name = "Todo Button", passwordMatched = true }) => {
   return (
     <button
-      className={` mt-2 rounded w-full border-2 px-6 py-2 font-semibold ${
+      className={`mt-2 rounded w-full border-2 px-6 py-2 font-semibold ${
         // if passwordMatched is true then button will be green
         // if passwordMatched is false then button will be gray
         passwordMatched
-          ? `custom-input border-green-500 text-green-500 active:bg-green-300 active:text-gray-600`
-          : `border-gray-400 text-gray-400 cursor-not-allowed`
-      } text-lg `}
-      type="submit"
-      // disabled={!passwordMatched} // button will activate only if passwordMatched is true
+          ? "border-green-500 text-green-500 active:bg-green-300 active:text-gray-600"
+          : "border-gray-400 text-gray-400 cursor-not-allowed"
+      } text-lg`}
+      disabled={!passwordMatched} // button will activate only if passwordMatched is true
     >
       {name}
     </button>
