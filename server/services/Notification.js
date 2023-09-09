8fc19269e18c8952f307d5da6d28cb3629b79966
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-09 11:11:24 +0530

    Notification.js : 1) added updateIsVerified functionality(To verify the Contact number of the user) 2) instead of fix contact number for otp verification, we are taking contact number from the user 3) Reminder is changed to Event

--- server/services/Notification.js
+++ server/services/Notification.js
@@ -3,9 +3,9 @@
 // require('dotenv').config({ path: './.env' });
 require("dotenv").config({ path: "./.env" });
 
-// const Reminder = require("../models/EventSchema");
-const Reminder = require("../models/EventSchema");
-
+// const Event = require("../models/EventSchema");
+const Event = require("../models/EventSchema");
+const User = require("../models/UserSchema");
 // WhatsApp API initialization
 
 const accountSid = process.env.ACCOUNT_SID;
@@ -14,16 +14,18 @@ const client = require("twilio")(accountSid, authToken);
 
 // function for sending whatsapp message
 exports.sendWhatsAppMessage = () => {
+  console.log("inside the sendWhatsAppMessage function");
   setInterval(() => {
+    console.log("inside the setInterval function");
     const now = new Date();
     const datetime = now.toISOString();
     // Get the current datetime in ISO format
     // match the exact date including the date, month, year, hours, minutes and seconds..
     console.log(datetime);
 
-    // Reminder.find({ remindAt: datetime, isReminded: false }).then((reminders) => {
-    Reminder.find({}).then((reminders) => {
-      // console.log("reminders: ", reminders);
+    // Event.find({ remindAt: datetime, isReminded: false }).then((reminders) => {
+    Event.find({}).then((reminders) => {
+      console.log("Total reminder available reminders: ", reminders);
       if (reminders) {
         // console.log("reminders: ", reminders);
 
@@ -40,14 +42,14 @@ exports.sendWhatsAppMessage = () => {
                 })
                 .then((message) => {
                   console.log("Message Id: ", message.sid);
-                  Reminder.findByIdAndUpdate(
+                  Event.findByIdAndUpdate(
                     reminder._id,
                     {
                       isReminded: true,
                     },
                     { new: true }
                   ).then((updatedReminder) => {
-                    console.log("Reminder updated: ", updatedReminder);
+                    console.log("Event updated: ", updatedReminder);
                   });
                 });
             }
@@ -61,17 +63,64 @@ exports.sendWhatsAppMessage = () => {
 exports.sendOtpEvent = async (req, res) => {
   console.log("req.body: ", req.body);
   const { contactNumber, Otp } = req.body;
+  console.log("*************************************************************");
   console.log("contactNumber: ", contactNumber);
   console.log("Otp: ", Otp);
-
+  console.log(
+    `type - ${typeof contactNumber} and contactNumber: ${contactNumber} 
+     type - ${typeof Otp} and otp: ${Otp} 
+    `
+  );
+  console.log("*************************************************************");
   client.messages
     .create({
-      body: "12345",
+      body: Otp,
       from: "+12565883356",
-      to: "+919881470684",
+      to: contactNumber,
     })
     .then((message) => console.log(message.sid));
   res.send("OTP sent successfully!");
 };
 
+// update the isVerified field of the user
+exports.updateIsVerified = async (req, res) => {
+  try {
+    // const { userId } = req.body;
+    const { userId, isVerified } = req.body;
+
+    if (!userId || typeof userId !== "string") {
+      if (!userId) {
+        throw new Error("userId is Required!");
+      } else {
+        throw new Error("userId should be of type String");
+      }
+    }
+
+    console.log("userId: ", userId);
+    const user = await User.findOne({ appwriteId: userId });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+    user.isVerified = true;
+
+    await user.save();
+    console.log("**************** inside the verify user:::::***********");
+    res.status(200).json({
+      success: true,
+      message: "User is verified successfully!",
+      data: user,
+    });
+
+    // if (user) { " " }
+  } catch (error) {
+    console.log("Error in updateIsVerified controller");
+    console.log("ERROR: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
 // module.exports= client;
