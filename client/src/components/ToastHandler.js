6c7bc2ccf772167c4a58e6d9b74dac25a64d86b9
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-11 15:52:19 +0530

    Added the ToastHandler in saparate files.. Yaaah.... WORA (write once use anywhere... 😂)

---
+++ client/src/components/ToastHandler.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const showToastLoading = () => {
+  return toast.loading("Please wait...");
+};
+
+export const showToastSuccess = (toastId) => {
+  toast.update(toastId, {
+    render: "All is good",
+    type: "success",
+    isLoading: false,
+    position: "top-center",
+    autoClose: 1000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
+export const showToastError = (errorMessage) => {
+  toast.error(errorMessage, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
+export const Toast = () => {
+  return (
+    <ToastContainer
+      position="top-center"
+      autoClose={1000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
+  );
+};
