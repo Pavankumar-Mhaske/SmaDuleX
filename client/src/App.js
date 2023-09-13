72e7c8d8dcd2238ea0c086ab732e3749aa8f2c4c
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-13 09:51:08 +0530

    App.js : All the components are lazy loaded...

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 
 /**
  * Importing react-router-dom components for routing the pages in application
@@ -9,14 +9,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
  * Importing pages which has to be assigned with route
  */
 
-// import HomePage from "./pages/Homepage";
-import HomePage from "./pages/Homepages";
-// import LoginPage from "./pages/LoginPage";
-import LoginPages from "./pages/LoginPages";
-// import SignupPage from "./pages/SignupPage";
-import SignupPages from "./pages/SignupPages";
-import WelcomePage from "./pages/WelcomePage";
-import GuidePages from "./pages/GuidePages";
+// // import HomePage from "./pages/Homepage";
+// import HomePage from "./pages/Homepages";
+// // import LoginPage from "./pages/LoginPage";
+// import LoginPages from "./pages/LoginPages";
+// // import SignupPage from "./pages/SignupPage";
+// import SignupPages from "./pages/SignupPages";
+// import WelcomePage from "./pages/WelcomePage";
+// import GuidePages from "./pages/GuidePages";
+
 /**
  * Importing layouts which has to be wrapped for every route
  */
@@ -29,6 +30,14 @@ import Footer from "./layouts/Footer";
  */
 import userContext from "./context/userContext";
 
+// lasy loading
+const HomePage = lazy(() => import("./pages/Homepages"));
+const LoginPages = lazy(() => import("./pages/LoginPages"));
+const SignupPages = lazy(() => import("./pages/SignupPages"));
+const WelcomePage = lazy(() => import("./pages/WelcomePage"));
+const GuidePages = lazy(() => import("./pages/GuidePages"));
+
+// app component
 const App = () => {
   /**
    * This will passed to userContext. Hence it is used verify if a user is logged in or not.
@@ -40,16 +49,18 @@ const App = () => {
       <userContext.Provider value={{ user, setUser }}>
         <Router>
           <Header />
-          <Routes>
-            <Route path="/" element={<WelcomePage />} />
-            {/* <Route path="/" element={<HomePage />} /> */}
-            <Route path="/home" element={<HomePage />} />
-            {/* <Route path="/login" element={<LoginPage />} /> */}
-            <Route path="/login" element={<LoginPages />} />
-            {/* <Route path="/signup" element={<SignupPage />} /> */}
-            <Route path="/signup" element={<SignupPages />} />
-            <Route path="/guide" element={<GuidePages />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<WelcomePage />} />
+              {/* <Route path="/" element={<HomePage />} /> */}
+              <Route path="/home" element={<HomePage />} />
+              {/* <Route path="/login" element={<LoginPage />} /> */}
+              <Route path="/login" element={<LoginPages />} />
+              {/* <Route path="/signup" element={<SignupPage />} /> */}
+              <Route path="/signup" element={<SignupPages />} />
+              <Route path="/guide" element={<GuidePages />} />
+            </Routes>
+          </Suspense>
           {/* <Footer /> */}
         </Router>
       </userContext.Provider>
