26c40e209bfb73d1867c78c22811ed9208e9afd6
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-04-16 23:39:44 +0530

    chore: just done origin

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ const cors = require("cors");
 app.use(
   cors({
     // origin: process.env.CORS_ORIGIN,
-    origin: [process.env.CORS_ORIGIN],
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
